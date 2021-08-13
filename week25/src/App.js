aa746a42d4e4be9df83284f17a11a0c224ef4fb4
DariaTsypkina <77797372+DariaTsypkina@users.noreply.github.com>
2021-08-13 14:11:06 +0300

    added pie chart

---
+++ week25/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Pie } from 'react-chartjs-2';
+
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [15, 18, 36, 5, 7, 12],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 2,
+    },
+  ],
+};
+
+const PieChart = () => (
+  <>
+    <div className='header'>
+      <h1 className='title'>Pie Chart</h1>
+    </div>
+    <Pie data={data} />
+  </>
+);
+
+export default PieChart;
