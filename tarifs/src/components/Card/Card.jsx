7e73c321bdcd3a24cb6fcd195807c0a0df3ae4fa
dariatsipkina <88425193+dariatsipkina@users.noreply.github.com>
2021-09-04 23:00:17 +0300

    added state

--- tarifs/src/components/Card/Card.jsx
+++ tarifs/src/components/Card/Card.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Card.module.scss';
 
-export default class Card extends React.Component {
-    render() {
-        const { price, speed, upper, priceContainer, chosen } = this.props;
-        return (
-            <div className={chosen !== false ? styles.card && chosen.card : styles.card}>
-                <div className={styles.card__titleContainer && upper}>
-                    <div className={styles.card__title}>Безлимитный {price}</div>
-                </div>
-                <div className={priceContainer}>руб <span className={styles.card__span}>{price}</span> /мес</div>
-                <div className={styles.card__info}>до {speed} Мбит/сек</div>
-                <div className={styles.card__footer}>Объем включенного трафика не ограничен</div>
-            </div>
-        )
+export default function Card(props) {
+    const [isSelected, toggleSelected] = useState(false);
+
+    const handleSelect = () => {
+        toggleSelected(!isSelected);
     }
+
+    const price = props.price;
+
+    const colorClass = (price === 300) ? styles.blue
+        : (price === 450) ? styles.green
+            : (price === 550) ? styles.red
+                : styles.black;
+
+    return (
+        <div className={isSelected ? styles.selected : styles.card} onClick={handleSelect}>
+            <div className={`${styles.card__titleContainer} ${colorClass}`}>
+                <div className={styles.card__title}>Безлимитный {price}</div>
+            </div>
+            <div className={`${styles.priceContainer} ${colorClass}`}>руб <span className={styles.card__span}>{props.price}</span> /мес</div>
+            <div className={styles.card__info}>до {props.speed} Мбит/сек</div>
+            <div className={styles.card__footer}>Объем включенного трафика не ограничен</div>
+        </div >
+    );
 }
--- tarifs/src/components/Cardlist/CardList.jsx
+++ tarifs/src/components/Cardlist/CardList.jsx
@@ -1,44 +1,44 @@
 import React from 'react';
 import Card from '../Card/Card';
 import styles from './CardList.module.scss';
-import CardBlack from '../Card/CardBlack.module.scss';
-import CardBlue from '../Card/CardBlue.module.scss';
-import CardRed from '../Card/CardRed.module.scss';
-import CardGreen from '../Card/CardGreen.module.scss';
-import ChosenCard from '../Card/ChosenCard.module.scss';
+
+const tarifs = [
+    {
+        price: 300,
+        speed: 10,
+        colorClass: 'blue'
+    },
+    {
+        price: 450,
+        speed: 50,
+        colorClass: 'green'
+    },
+    {
+        price: 550,
+        speed: 100,
+        colorClass: 'red'
+    },
+    {
+        price: 1000,
+        speed: 200,
+        colorClass: 'black'
+    }
+];
 
 export default class CardList extends React.Component {
     render() {
         return (
             <div className={styles.cardList}>
-                <Card
-                    price="300"
-                    speed="10"
-                    upper={CardBlue.blue}
-                    priceContainer={CardBlue.priceContainer}
-                    chosen={false}
-                />
-                <Card
-                    price="450"
-                    speed="50"
-                    upper={CardGreen.green}
-                    priceContainer={CardGreen.priceContainer}
-                    chosen={false}
-                />
-                <Card
-                    price="550"
-                    speed="100"
-                    upper={CardRed.red}
-                    priceContainer={CardRed.priceContainer}
-                    chosen={ChosenCard}
-                />
-                <Card
-                    price="1000"
-                    speed="200"
-                    upper={CardBlack.black}
-                    priceContainer={CardBlack.priceContainer}
-                    chosen={false}
-                />
+                {
+                    tarifs.map(tarif =>
+                        <Card
+                            key={tarif.price}
+                            price={tarif.price}
+                            speed={tarif.speed}
+                            color={tarif.colorClass}
+                        />
+                    )
+                }
             </div>
         );
     }
